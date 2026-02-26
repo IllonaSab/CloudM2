b00b6616ff7e352c4216533b44bc639311ad4b17
IllonaSab <sabouillona@gmail.com>
2026-02-26 16:27:31 +0100

    fix

--- src/api/app/config.py
+++ src/api/app/config.py
@@ -1,6 +1,8 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_file=".env", extra="ignore")
+
     cosmos_endpoint: str
     cosmos_key: str
     cosmos_database: str = "db-doc"
