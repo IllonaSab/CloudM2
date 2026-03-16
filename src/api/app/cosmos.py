09d4a5bcc9ba94675c069a6572b52535abf57f2b
IllonaSab <sabouillona@gmail.com>
2026-03-16 21:56:19 +0100

    again

--- src/api/app/cosmos.py
+++ src/api/app/cosmos.py
@@ -2,15 +2,19 @@ from azure.cosmos import CosmosClient
 from .config import settings
 
 _client = None
+_container = None
+
 
 def get_cosmos_container():
-    global _client
+    global _client, _container
 
-    if _client is None:
+    if _container is None:
         _client = CosmosClient(
             settings.COSMOS_ENDPOINT,
             credential=settings.COSMOS_KEY
         )
 
-    database = _client.get_database_client(settings.COSMOS_DATABASE)
-    return database.get_container_client(settings.COSMOS_CONTAINER)
+        database = _client.get_database_client(settings.COSMOS_DATABASE)
+        _container = database.get_container_client(settings.COSMOS_CONTAINER)
+
+    return _container
