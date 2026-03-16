36bcfedd90743e31de0ee8b9f235293a8f6225a5
IllonaSab <sabouillona@gmail.com>
2026-03-16 11:21:15 +0100

    front ready

--- src/api/app/config.py
+++ src/api/app/config.py
@@ -1,7 +1,7 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
-    model_config = SettingsConfigDict(env_file=".env", extra="ignore")
+    model_config = SettingsConfigDict(case_sensitive=False)
 
     cosmos_endpoint: str
     cosmos_key: str
--- src/front/src/App.jsx
+++ src/front/src/App.jsx
@@ -5,49 +5,69 @@ const API_URL = "https://api-doc-is-hgg7g4d9e2fagpgt.francecentral-01.azurewebsi
 function App() {
   const [jobId, setJobId] = useState(null);
   const [file, setFile] = useState(null);
+  const [message, setMessage] = useState("");
 
   const createJob = async () => {
-    const response = await fetch(`${API_URL}/jobs`, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ name: "New Job" })
-    });
+    try {
+      const response = await fetch(`${API_URL}/jobs`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ name: "New Job" })
+      });
 
-    const data = await response.json();
-    setJobId(data.id);
+      const data = await response.json();
+      setJobId(data.id);
+      setMessage("Job créé !");
+    } catch (error) {
+      console.error(error);
+      setMessage("Erreur création job");
+    }
   };
 
   const uploadFile = async () => {
     if (!file || !jobId) return;
 
-    const response = await fetch(`${API_URL}/jobs/${jobId}/upload-url`);
-    const { uploadUrl } = await response.json();
+    try {
+      const response = await fetch(`${API_URL}/jobs/${jobId}/upload-url`);
+      const { uploadUrl } = await response.json();
 
-    await fetch(uploadUrl, {
-      method: "PUT",
-      headers: { "x-ms-blob-type": "BlockBlob" },
-      body: file
-    });
+      await fetch(uploadUrl, {
+        method: "PUT",
+        headers: { "x-ms-blob-type": "BlockBlob" },
+        body: file
+      });
 
-    alert("Upload terminé !");
+      setMessage("Upload terminé !");
+    } catch (error) {
+  console.error(error);
+  setMessage("Erreur création job");
+}
   };
 
   return (
     <div style={{ padding: 40 }}>
       <h1>CloudM2 Front</h1>
 
-      <button onClick={createJob}>Créer un Job</button>
+      <button onClick={createJob}>
+        Créer un Job
+      </button>
 
       {jobId && (
         <>
-          <p>Job ID: {jobId}</p>
+          <p>Job ID : {jobId}</p>
+
           <input
             type="file"
             onChange={(e) => setFile(e.target.files[0])}
           />
-          <button onClick={uploadFile}>Uploader</button>
+
+          <button onClick={uploadFile}>
+            Uploader
+          </button>
         </>
       )}
+
+      <p>{message}</p>
     </div>
   );
 }
