f763120a952f0e7c76a916fc78521047a6534800
IllonaSab <sabouillona@gmail.com>
2026-03-16 10:21:31 +0100

    fix cors

--- src/api/app/main.py
+++ src/api/app/main.py
@@ -1,12 +1,22 @@
 from fastapi import FastAPI
 from azure.storage.blob import BlobServiceClient
 from .config import settings
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(
     title="Doc processing API",
     description="API de génération de documents",
     version="1.0.0"
 )
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # pour dev
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+app = FastAPI()
 
 @app.on_event("startup")
 def startup_event():
