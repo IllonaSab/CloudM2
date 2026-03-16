844c5bd70ce32ec8f69c416686ccce902b93dc1f
IllonaSab <sabouillona@gmail.com>
2026-03-16 12:39:51 +0100

    cors

--- src/api/app/main.py
+++ src/api/app/main.py
@@ -1,34 +1,22 @@
 from fastapi import FastAPI
-from azure.storage.blob import BlobServiceClient
-from .config import settings
 from fastapi.middleware.cors import CORSMiddleware
+from .routes_jobs import router as jobs_router
 
-app = FastAPI(
-    title="Doc processing API",
-    description="API de génération de documents",
-    version="1.0.0"
-)
+app = FastAPI()
 
+# 🔥 AJOUTE CECI
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # pour dev
+    allow_origins=[
+        "http://localhost:5173",
+        "https://cloudm2-front.azurewebsites.net"  # ton front Azure
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-app = FastAPI()
-
-@app.on_event("startup")
-def startup_event():
-    if not settings.blob_connection_string:
-        raise ValueError("BLOB_CONNECTION_STRING not set")
-
-    app.state.blob_service = BlobServiceClient.from_connection_string(
-        settings.blob_connection_string
-    )
 
 app.include_router(jobs_router)
-
 
 @app.get("/health")
 def health():
